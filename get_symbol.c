8930022c9697340d5565a2178bb932b34801effa
Kim-Yongjun <dydwns0324@gmail.com>
2019-04-28 17:01:46 +0900

    modifying get_symbol.c

--- get_symbol.c
+++ get_symbol.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef enum
 {S_WORD, S_BAR, S_AMP, S_SEMI, S_GT, S_GTGT, S_LT, S_NL, S_EOF} SYMBOL;
@@ -12,7 +13,7 @@ SYMBOL getsymb(char *word){
 	int c;
 	char *w;
 
-	status = NEUTRAL; //state initialization.
+	state = NEUTRAL; //state initialization.
 	w = word; //preserved the value of word at the end of the function.
 
 	while((c=getchar()) != EOF){
@@ -42,4 +43,111 @@ SYMBOL getsymb(char *word){
 						state = INQUOTE;
 						continue;
 					default :
-						state = INWORD	
+						state = INWORD;
+						*w++ = c;
+						continue;
+				}
+			case INQUOTE:
+				switch (c){
+					case '\\' : 
+					       *w++ = getchar();
+				       		continue;
+				 	case '"' :
+						*w = '\0';
+						return S_WORD;
+					default :
+						*w++ = c;
+						continue;
+				}
+			case INWORD:
+				switch (c){
+					case ';' :
+					case '&' :
+					case '|' : 
+					case '<' :
+					case '>' :
+					case '\n' :
+					case ' ' :
+					case '\t' :
+						ungetc(c,stdin);
+						*w = '\0';
+						return S_WORD;
+					default:
+						*w++ = c;
+						continue;
+				}
+		}
+	}
+	return S_EOF;
+}
+
+
+int main(){
+	char word[200];
+
+	while (1){
+		switch(getsymb(word)){
+			case S_WORD:
+				printf("S_WORD<%s>\n", word);
+				break;
+			case S_BAR:
+				printf("S_BAR\n");
+				break;
+			case S_AMP:
+				printf("S_AMP\n");
+				break;
+			case S_SEMI:
+				printf("S_SEMI\n");
+				break;
+			case S_GT:
+				printf("S_GT\n");
+				break;
+			case S_GTGT:
+				printf("S_GTGT\n");
+				break;
+			case S_LT:
+				printf("S_LT\n");
+				break;
+			case S_NL:
+				printf("S_LN\n");
+				break;
+			case S_EOF:
+				printf("S_EOF\n");
+				exit(0);
+		}
+	return 0;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
